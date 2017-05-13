5fd07c69cc1c25a9ec5885678291d38bc7ce6874
Chao <hyrchao@outlook.com>
2017-05-13 20:31:03 +0800

    Add ItemPickup class

---
+++ Source/ARG/ItemPickup.h
@@ -0,0 +1,43 @@
+//2017/05/13
+//By Chao
+//拾取物品类
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ItemPickup.generated.h"
+
+UCLASS()
+
+class ARG_API AItemPickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	//构造函数
+	AItemPickup();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
+	bool isActive;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
+	USphereComponent* collision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
+	UStaticMeshComponent* pickupMesh;
+
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnPickedUp();
+
+
+
+protected:
+	
+	virtual void BeginPlay() override;
+	
+private:
+
+};
